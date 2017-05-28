e6168119e3c976c9837e1e8110ce253045cc6d8e
Bhavin <bhavingpt@gmail.com>
2017-05-27 19:45:45 -0700

    setup is on the way

---
+++ mnemosyne/CardTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CardTableViewCell.swift
+//  mnemosyne
+//
+//  Created by Bhavin Gupta on 5/27/17.
+//  Copyright © 2017 bhavingpt. All rights reserved.
+//
+
+import UIKit
+
+class CardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
