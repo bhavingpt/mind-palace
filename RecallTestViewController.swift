9e81ac6e564c97d25c34eb7f70e9c32d19eea217
Bhavin <bhavingpt@gmail.com>
2017-05-31 22:20:46 -0700

    recall test working

--- RecallTestViewController.swift
+++ RecallTestViewController.swift
@@ -17,6 +17,10 @@ class RecallTestViewController: UIViewController {
     var cleanMapping = [String]()
     var test = [Int]()
     var results = [Bool]()
+    let images = [#imageLiteral(resourceName: "card0"), #imageLiteral(resourceName: "card1"), #imageLiteral(resourceName: "card2"), #imageLiteral(resourceName: "card3"), #imageLiteral(resourceName: "card4"), #imageLiteral(resourceName: "card5"), #imageLiteral(resourceName: "card6"), #imageLiteral(resourceName: "card7"), #imageLiteral(resourceName: "card8"), #imageLiteral(resourceName: "card9"), #imageLiteral(resourceName: "card10"), #imageLiteral(resourceName: "card11"), #imageLiteral(resourceName: "card12"),
+                  #imageLiteral(resourceName: "card13"), #imageLiteral(resourceName: "card14"), #imageLiteral(resourceName: "card15"), #imageLiteral(resourceName: "card16"), #imageLiteral(resourceName: "card17"), #imageLiteral(resourceName: "card18"), #imageLiteral(resourceName: "card19"), #imageLiteral(resourceName: "card20"), #imageLiteral(resourceName: "card21"), #imageLiteral(resourceName: "card22"), #imageLiteral(resourceName: "card23"), #imageLiteral(resourceName: "card24"), #imageLiteral(resourceName: "card25"),
+                  #imageLiteral(resourceName: "card26"), #imageLiteral(resourceName: "card27"), #imageLiteral(resourceName: "card28"), #imageLiteral(resourceName: "card29"), #imageLiteral(resourceName: "card30"), #imageLiteral(resourceName: "card31"), #imageLiteral(resourceName: "card32"), #imageLiteral(resourceName: "card33"), #imageLiteral(resourceName: "card34"), #imageLiteral(resourceName: "card35"), #imageLiteral(resourceName: "card36"), #imageLiteral(resourceName: "card37"), #imageLiteral(resourceName: "card38"),
+                  #imageLiteral(resourceName: "card39"), #imageLiteral(resourceName: "card40"), #imageLiteral(resourceName: "card41"), #imageLiteral(resourceName: "card42"), #imageLiteral(resourceName: "card43"), #imageLiteral(resourceName: "card44"), #imageLiteral(resourceName: "card45"), #imageLiteral(resourceName: "card46"), #imageLiteral(resourceName: "card47"), #imageLiteral(resourceName: "card48"), #imageLiteral(resourceName: "card49"), #imageLiteral(resourceName: "card50"), #imageLiteral(resourceName: "card51")]
     
     var question: Int = 0
     
@@ -36,6 +40,10 @@ class RecallTestViewController: UIViewController {
     @IBOutlet weak var choiceC: UIButton!
     @IBOutlet weak var choiceD: UIButton!
     @IBOutlet weak var stopwatch: UILabel!
+    @IBOutlet weak var clock: UILabel!
+    @IBOutlet var textFields: [UILabel]!
+    @IBOutlet weak var numCorrect: UILabel!
+    @IBOutlet weak var numTotal: UILabel!
     
     // MARK: Timer
     
@@ -47,7 +55,20 @@ class RecallTestViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        for button in [choiceA, choiceB, choiceC, choiceD] {
+            button?.layer.borderColor = UIColor.blue.cgColor
+            button?.layer.borderWidth = 1
+            button?.layer.cornerRadius = 5
+        }
+        
         if test.count == 0 {
+            clock.isHidden = true
+            for label in textFields {
+                label.isHidden = true
+            }
+            numCorrect.isHidden = true
+            numTotal.isHidden = true
+            
             self.mapping = (defaults.object(forKey: "pao") as? [String])!
             
             for i in 0...51 {
@@ -66,21 +87,17 @@ class RecallTestViewController: UIViewController {
     // MARK: Actions
     
     @IBAction func selection(_ sender: UIButton) {
-        results.append(sender.currentTitle! == cleanMapping[question - 1])
+        results.append(sender.currentTitle! == cleanMapping[test[question - 1]])
         if results.last! {
-            self.view.backgroundColor = UIColor.green
-            print("LOL")
+            sender.backgroundColor = UIColor.green
+            sender.setNeedsDisplay()
         } else {
-            self.view.backgroundColor = UIColor.red
-            print( "HM")
+            sender.backgroundColor = UIColor.red
+            sender.setNeedsDisplay()
         }
         
-        self.view.setNeedsDisplay()
-            
-        usleep(50000)
+        sender.setNeedsDisplay()
         
-        self.view.backgroundColor = UIColor.white
-        self.view.setNeedsDisplay()
         nextQuestion()
     }
     
@@ -137,21 +154,36 @@ class RecallTestViewController: UIViewController {
             var mult = [card, fake1, fake2, fake3]
             mult = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: mult) as! [Int]
             
+            while ( match(newarray: mult) ) {
+                mult = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: mult) as! [Int]
+            }
+            
             choiceA.setTitle(cleanMapping[mult[0]], for: .normal)
             choiceB.setTitle(cleanMapping[mult[1]], for: .normal)
             choiceC.setTitle(cleanMapping[mult[2]], for: .normal)
             choiceD.setTitle(cleanMapping[mult[3]], for: .normal)
             
-            // TODO handle the image
-            
+            image.image = images[test[question - 1]]
         }
+        for button in [choiceA, choiceB, choiceC, choiceD] {
+            button?.backgroundColor = UIColor.white
+            button?.setNeedsDisplay()
+        }
+    }
+    
+    private func match(newarray: [Int]) -> Bool {
+        var buttons = [choiceA, choiceB, choiceC, choiceD]
+        for i in 0...3 {
+            if buttons[i]?.titleLabel?.text == cleanMapping[newarray[i]] {
+                return true
+            }
+        }
+        return false
     }
     
     private func endTest() {
         elapsed = Date().timeIntervalSinceReferenceDate - startTime
         timer?.invalidate()
-        
-        // TODO display the number correct
         
         choiceA.isHidden = true
         choiceB.isHidden = true
@@ -159,7 +191,23 @@ class RecallTestViewController: UIViewController {
         choiceD.isHidden = true
         image.isHidden = true
         number.isHidden = true
-        stopwatch.textColor = UIColor.green
+        stopwatch.isHidden = true
+        
+        clock.text = stopwatch.text
+        clock.isHidden = false
+        for label in textFields {
+            label.isHidden = false
+        }
+        
+        var right = 0
+        for q in results {
+            right += q ? 1 : 0
+        }
+        
+        numCorrect.text = String(right)
+        numCorrect.isHidden = false
+        numTotal.text = String(question)
+        numTotal.isHidden = false
     }
     
     private func isTested(cardNum: Int) -> Bool {
